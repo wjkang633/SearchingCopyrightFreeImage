94ced9274e737219be2f59f922b9d7da8f9cc0b3
kwjjjjj <kwjjjjj@abrain.co.kr>
2021-10-21 17:34:13 +0900

    [added] 메인에서 어댑터 호출

--- app/src/main/java/woojin/android/kotlin/project/searchingcopyrightfreeimage/MainActivity.kt
+++ app/src/main/java/woojin/android/kotlin/project/searchingcopyrightfreeimage/MainActivity.kt
@@ -2,18 +2,27 @@ package woojin.android.kotlin.project.searchingcopyrightfreeimage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import woojin.android.kotlin.project.searchingcopyrightfreeimage.data.Repository
+import woojin.android.kotlin.project.searchingcopyrightfreeimage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private val scope = MainScope()
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        initViews()
         fetchRandomPhotos()
     }
 
@@ -23,8 +32,17 @@ class MainActivity : AppCompatActivity() {
         scope.cancel()
     }
 
+    private fun initViews() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.recyclerView.adapter = PhotoAdapter()
+    }
+
     private fun fetchRandomPhotos(query: String? = null) = scope.launch {
         Repository.getRandomPhotos(query)?.let { photos ->
+            (binding.recyclerView.adapter as? PhotoAdapter)?.apply {
+                this.photos = photos
+                notifyDataSetChanged()
+            }
         }
     }
 }
