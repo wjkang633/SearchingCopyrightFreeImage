44db0f1ce630e9a073415b86803f2fc692d9dbfd
kwjjjjj <kwjjjjj@abrain.co.kr>
2021-10-22 15:31:14 +0900

    [added] 에러 화면 처리/API 호출 예외 처리/페이스북 ShimmerLayout 사용

--- app/src/main/java/woojin/android/kotlin/project/searchingcopyrightfreeimage/MainActivity.kt
+++ app/src/main/java/woojin/android/kotlin/project/searchingcopyrightfreeimage/MainActivity.kt
@@ -1,8 +1,12 @@
 package woojin.android.kotlin.project.searchingcopyrightfreeimage
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.LinearLayout
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.MainScope
@@ -23,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initViews()
+        bindViews()
         fetchRandomPhotos()
     }
 
@@ -37,12 +42,47 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.adapter = PhotoAdapter()
     }
 
-    private fun fetchRandomPhotos(query: String? = null) = scope.launch {
-        Repository.getRandomPhotos(query)?.let { photos ->
-            (binding.recyclerView.adapter as? PhotoAdapter)?.apply {
-                this.photos = photos
-                notifyDataSetChanged()
+    private fun bindViews() {
+        //검색바
+        binding.searchEditText.setOnEditorActionListener { editText, actionId, event ->
+            //키보드 내리기&포커스 날리기
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                currentFocus?.let { view ->
+                    val inputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+
+                    view.clearFocus()
+                }
+
+                fetchRandomPhotos(editText.text.toString())
             }
+
+//            return@setOnEditorActionListener true
+            true
+        }
+
+        binding.refreshLayout.setOnRefreshListener {
+            fetchRandomPhotos(binding.searchEditText.text.toString())
+        }
+    }
+
+    private fun fetchRandomPhotos(query: String? = null) = scope.launch {
+        try {
+            Repository.getRandomPhotos(query)?.let { photos ->
+                (binding.recyclerView.adapter as? PhotoAdapter)?.apply {
+                    this.photos = photos
+                    notifyDataSetChanged()
+                }
+            }
+            binding.recyclerView.isVisible = true
+            binding.errorDescriptionTextView.isVisible = false
+        } catch (exception: Exception) {
+            binding.recyclerView.isVisible = false
+            binding.errorDescriptionTextView.isVisible = true
+        } finally {
+            binding.shimmerLayout.isVisible = false
+            binding.refreshLayout.isRefreshing = false
         }
     }
 }
--- app/src/main/java/woojin/android/kotlin/project/searchingcopyrightfreeimage/PhotoAdapter.kt
+++ app/src/main/java/woojin/android/kotlin/project/searchingcopyrightfreeimage/PhotoAdapter.kt
@@ -9,6 +9,7 @@ import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import woojin.android.kotlin.project.searchingcopyrightfreeimage.data.models.PhotoResponse
 import woojin.android.kotlin.project.searchingcopyrightfreeimage.databinding.ItemPhotoBinding
 
+
 class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
 
     var photos: List<PhotoResponse> = emptyList()
@@ -22,28 +23,28 @@ class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
             val targetHeight = (targetWidth * dimensionRation).toInt()
 
             binding.contentsContainer.layoutParams =
-                binding.contentsContainer.layoutParams.apply {
-                height = targetHeight
-            }
+                    binding.contentsContainer.layoutParams.apply {
+                        height = targetHeight
+                    }
 
             //기본 이미지
             Glide.with(binding.root)
-                .load(photo.urls?.regular)
-                .thumbnail(
-                    Glide.with(binding.root)
-                        .load(photo.urls?.thumb)
-                        .transition(DrawableTransitionOptions.withCrossFade())
-                )
-                .override(targetWidth, targetHeight)
-                .into(binding.photoImageView)
+                    .load(photo.urls?.regular)
+                    .thumbnail(
+                            Glide.with(binding.root)
+                                    .load(photo.urls?.thumb)
+                                    .transition(DrawableTransitionOptions.withCrossFade())
+                    )
+                    .override(targetWidth, targetHeight)
+                    .into(binding.photoImageView)
 
             //프로필 이미지
             Glide.with(binding.root)
-                .load(photo.user?.profileImageUrls?.small)
-                .placeholder(R.drawable.shape_profile_placeholder)
-                .circleCrop()
-                .transition(DrawableTransitionOptions.withCrossFade())
-                .into(binding.profileImageView)
+                    .load(photo.user?.profileImageUrls?.small)
+                    .placeholder(R.drawable.shape_profile_placeholder)
+                    .circleCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(binding.profileImageView)
 
             if (photo.user?.name.isNullOrBlank()) {
                 binding.authorTextView.isVisible = false
@@ -62,13 +63,13 @@ class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-        ViewHolder(
-            ItemPhotoBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
+            ViewHolder(
+                    ItemPhotoBinding.inflate(
+                            LayoutInflater.from(parent.context),
+                            parent,
+                            false
+                    )
             )
-        )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(photos[position])
